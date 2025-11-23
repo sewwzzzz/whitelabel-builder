2d54020c469922c8ecb26bd6a31ad80ba1ecea05
sewwzzzz <1192963064@qq.com>
2025-11-23 19:38:45 +0800

    feat:Naive UI component import

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,34 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// 自动导入API（vue/naive ai的工具函数）
+import AutoImport from 'unplugin-auto-import/vite';
+// 自动加载组件
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite';
+
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar',
+          ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
   ],
   resolve: {
     alias: {
